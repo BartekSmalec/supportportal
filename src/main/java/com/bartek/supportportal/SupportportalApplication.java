16a7e51e481d5a5ec7a0d4b1d10f257b5831752d
bartek <smalecbartlomiej@gmail.com>
2023-02-15 12:39:20 +0100

    Created entity + mysql config

--- src/main/java/com/bartek/supportportal/SupportportalApplication.java
+++ src/main/java/com/bartek/supportportal/SupportportalApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SupportportalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SupportportalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SupportportalApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/bartek/supportportal/resource/UserResource.java
@@ -0,0 +1,15 @@
+package com.bartek.supportportal.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    @GetMapping("/test")
+    public String test() {
+        return "app works";
+    }
+}
