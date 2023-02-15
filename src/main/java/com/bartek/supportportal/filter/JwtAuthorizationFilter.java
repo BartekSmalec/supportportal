ff24d643ead051bef8495f28a91b2b47bb767b25
bartek <smalecbartlomiej@gmail.com>
2023-02-15 15:22:01 +0100

    Added JWT authorization token filter

---
+++ src/main/java/com/bartek/supportportal/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,53 @@
+package com.bartek.supportportal.filter;
+
+import com.bartek.supportportal.utilty.JwtTokenProvider;
+import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import static com.bartek.supportportal.constant.SecurityConstant.OPTIONS_HTTP_METHOD;
+import static com.bartek.supportportal.constant.SecurityConstant.TOKEN_PREFIX;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.OK;
+
+@AllArgsConstructor
+@Component
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getMethod().equalsIgnoreCase(OPTIONS_HTTP_METHOD)) {
+            response.setStatus(OK.value());
+        } else {
+            String authorizationHeader = request.getHeader(AUTHORIZATION);
+            if (StringUtils.isEmpty(authorizationHeader) || !StringUtils.startsWith(authorizationHeader, TOKEN_PREFIX)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String token = authorizationHeader.substring(TOKEN_PREFIX.length());
+            String username = jwtTokenProvider.getSubject(token);
+            if (jwtTokenProvider.isTokenValid(username, token)) {
+                List<GrantedAuthority> authorities = jwtTokenProvider.getAuthorities(token);
+                Authentication authentication = jwtTokenProvider.getAuthentication(username, authorities, request);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                SecurityContextHolder.clearContext();
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
